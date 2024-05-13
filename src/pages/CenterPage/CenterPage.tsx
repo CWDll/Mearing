c2ac7984e5008827f4fb1844d061d6e462a6c6d8
이창준 <2018110021@dongguk.edu>
2024-05-13 18:17:32 +0900

    [Design] centerPage 헤더 추가

--- src/pages/CenterPage/CenterPage.tsx
+++ src/pages/CenterPage/CenterPage.tsx
@@ -1,18 +1,24 @@
 import React from "react";
+import * as S from "./style"
 import { useNavigate } from "react-router-dom";
+
+// 컴포넌트 가져오기
+import TitleBar from "../../components/TitleBar";
 
 const CenterPage: React.FC = () => {
   const navigate = useNavigate();
 // 뒤로가기 버튼 클릭 이벤트
-const goToBack = (): void => {
-  navigate("/");
-};
+  const goToBack = (): void => {
+    navigate("/");
+  };
 
   return (
-    <div>
-      <h1>Center Page</h1>
-      <button onClick={goToBack}>Go to Main Page</button>
-    </div>
+    <S.CenterContainer>
+      <S.Header>
+        <div>back</div>
+        <TitleBar title="도움 시설"/>
+      </S.Header>
+    </S.CenterContainer>
   );
 };
 
--- src/pages/CenterPage/style.tsx
+++ src/pages/CenterPage/style.tsx
@@ -1,1 +1,18 @@
-export {};
+import styled from "styled-components";
+
+export const CenterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  justify-content: space-around;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  height: 5em;
+  border: 1px solid black;
+  `;
