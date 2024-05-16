2b900d79c7e81955ed8b8086e62476680cebf85b
이창준 <2018110021@dongguk.edu>
2024-05-16 19:38:11 +0900

    [Style] 채팅 메시지 컴포넌트 주석 추가

--- src/components/ChatbotMsgBox.tsx
+++ src/components/ChatbotMsgBox.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import styled, { css } from "styled-components";
 
+// 메시지 박스 타입 정의
 interface ChatbotMsgBox {
   writer: string;
   date: string;
   content: string;
 }
+
+// 속성 중 writer의 값 지정
 interface ChatbotMsgBoxProps extends ChatbotMsgBox {
   writer: "person" | "gpt";
 }
 
-// 스타일을 조건부로 적용하는 함수
+// 채팅 전송자에 따른 스타일 분리
 const messageBoxStyle = css<{ writer: "person" | "gpt" }>`
   border: 1px solid
     ${({ writer }) => (writer === "person" ? "#4CAF50" : "#2196F3")};
@@ -20,6 +23,7 @@ const messageBoxStyle = css<{ writer: "person" | "gpt" }>`
     writer === "person" ? "margin-left: auto;" : "margin-right: auto;"}
 `;
 
+// 채팅 메세지의 공통 스타일
 const ContentBoxWrapper = styled.div<{ writer: "person" | "gpt" | any }>`
   display: flex;
   flex-direction: column;
