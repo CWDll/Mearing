ed08c77465d78332c9acf469115493da02f59da3
이창준 <2018110021@dongguk.edu>
2024-05-31 18:36:44 +0900

    [Refactor] index.css내용을 global.ts파일로 통합시킨다.

--- src/shared/global.ts
+++ src/shared/global.ts
@@ -3,8 +3,24 @@ import reset from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
   ${reset};
-  max-width: 768px;
-  max-height: 1080px;
-  max-width: 100%;
-  background-color: blue;
-`;
+  
+  body {
+    @import url("https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap");
+
+    // margin: 0;
+    /* font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif; */
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  
+    font-family: "Do Hyeon", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  
+    background-color: #efefeb;
+    max-width: 768px;
+    max-height: 1080px;
+    }
+  }
+  `;
