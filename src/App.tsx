08cad9259cdb3a4ad79748b6ff569d36fba8b371
이창준 <2018110021@dongguk.edu>
2024-06-22 20:21:19 +0900

    [Feat] Route에 로그인, 회원가입 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import CenterPage from "./pages/CenterPage/CenterPage";
 import ChatbotPage from "./pages/ChatbotPage/ChatbotPage";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/main" element={<MainPage />} />
         <Route path="/center" element={<CenterPage />} />
         <Route path="/chatbot" element={<ChatbotPage />} />
       </Routes>
